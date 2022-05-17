5c3c67fe52da07181771fad2c291a5106a4d3e80
ParamithaGunawan <a2451f3058@bangkit.academy>
2022-05-17 23:53:18 +0700

    main activity

--- app/src/main/java/com/example/chanity/MainActivity.kt
+++ app/src/main/java/com/example/chanity/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.chanity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*var Toolbar = R.id.toolbar*//*
+        setSupportActionBar(findViewById(R.id.toolbar))*/
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.language -> {
+                startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+                return true
+            }
+            R.id.logout -> {
+                /*MainViewModel.logout()*/
+                return true
+            }
+            else -> true
+        }
     }
 }
