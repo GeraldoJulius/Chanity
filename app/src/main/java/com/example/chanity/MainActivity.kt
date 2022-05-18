d5c916113c59862d3a0415a45c79277a876afb3a
ParamithaGunawan <a2451f3058@bangkit.academy>
2022-05-18 21:10:50 +0700

    layout

--- app/src/main/java/com/example/chanity/MainActivity.kt
+++ app/src/main/java/com/example/chanity/MainActivity.kt
@@ -7,33 +7,43 @@ import android.provider.Settings
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
+import com.example.chanity.databinding.ActivityMainBinding
+import com.example.chanity.databinding.ActivitySigninBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        /*var Toolbar = R.id.toolbar*//*
-        setSupportActionBar(findViewById(R.id.toolbar))*/
+        setupAction()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.option_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.language -> {
-                startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
-                return true
-            }
-            R.id.logout -> {
-                /*MainViewModel.logout()*/
-                return true
-            }
-            else -> true
+    private fun setupAction() {
+        binding.button.setOnClickListener {
+            startActivity(Intent(this, TestActivity::class.java))
         }
     }
+
+        override fun onCreateOptionsMenu(menu: Menu): Boolean {
+            val inflater = menuInflater
+            inflater.inflate(R.menu.option_menu, menu)
+            return true
+        }
+
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            return when (item.itemId) {
+                R.id.language -> {
+                    startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+                    return true
+                }
+                R.id.logout -> {
+                    /*MainViewModel.logout()*/
+                    return true
+                }
+                else -> true
+            }
+        }
 }
