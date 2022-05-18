97e07a5848805b36e7cc741a1a22b94ca07748d7
ParamithaGunawan <a2451f3058@bangkit.academy>
2022-05-18 21:31:05 +0700

    .

--- app/src/main/java/com/example/chanity/ResultActivity.kt
+++ app/src/main/java/com/example/chanity/ResultActivity.kt
@@ -1,11 +1,45 @@
 package com.example.chanity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
+import com.example.chanity.databinding.ActivityResultBinding
+import com.example.chanity.databinding.ActivityTestBinding
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_menu2, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.language -> {
+                startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+                return true
+            }
+            R.id.logout -> {
+                /*MainViewModel.logout()*/
+                return true
+            }
+
+            R.id.home -> {
+                startActivity(Intent(this, MainActivity::class.java))
+                return true
+            }
+            else -> true
+        }
     }
 }
--- app/src/main/java/com/example/chanity/TestActivity.kt
+++ app/src/main/java/com/example/chanity/TestActivity.kt
@@ -1,11 +1,48 @@
 package com.example.chanity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
+import com.example.chanity.databinding.ActivityMainBinding
+import com.example.chanity.databinding.ActivityTestBinding
 
 class TestActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTestBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
+        binding = ActivityTestBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupAction()
+    }
+
+    private fun setupAction() {
+        binding.button2.setOnClickListener {
+            startActivity(Intent(this, ResultActivity::class.java))
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.language -> {
+                startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+                return true
+            }
+            R.id.logout -> {
+                /*MainViewModel.logout()*/
+                return true
+            }
+            else -> true
+        }
     }
 }
