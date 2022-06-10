3af616e85a06e94d2a9ffc98e4f754d974eab10a
GeraldoJulius <a2451f3056@bangkit.academy>
2022-06-10 17:36:02 +0700

    ModelTestResult
    implementasi model machinelearning
    dan menampilkan hasil test

--- app/src/main/java/com/example/chanity/ui/result/ResultActivity.kt
+++ app/src/main/java/com/example/chanity/ui/result/ResultActivity.kt
@@ -8,11 +8,13 @@ import android.os.Bundle
 import android.provider.Settings
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import com.example.chanity.R
 import com.example.chanity.databinding.ActivityResultBinding
 import com.example.chanity.ml.Model
 import com.example.chanity.ui.main.MainActivity
+import com.example.chanity.ui.test.TestActivity2
 import com.example.chanity.ui.welcome.WelcomeActivity
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -24,6 +26,7 @@ import java.nio.ByteBuffer
 class ResultActivity : AppCompatActivity() {
     private lateinit var binding: ActivityResultBinding
     private lateinit var resultViewModel: ResultViewModel
+    private lateinit var resultModel : TestActivity2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,48 +35,65 @@ class ResultActivity : AppCompatActivity() {
 
         resultViewModel = ViewModelProvider(this).get(ResultViewModel::class.java)
 
-        setupViewModel()
+
+        getResult()
     }
 
-    @SuppressLint("StringFormatInvalid")
-    private fun setupViewModel() {
-        resultViewModel.postQuiz()
+private fun getResult(){
 
-        resultViewModel.quizResponse.observe(this) {
+    val bundle = intent.extras
+    val traits1 = bundle?.getFloat("traits")
+    val traits2 = bundle?.getFloat("traits1")
+    val traits3 = bundle?.getFloat("traits2")
+    val traits4 = bundle?.getFloat("traits3")
+    val traits5 = bundle?.getFloat("traits4")
 
-        }
+
+
+    val traits = listOf(traits1,traits2,traits3,traits4,traits5)
+
+    val maxIdx = traits.indices.maxByOrNull { traits[it]!! }
+
+    if (maxIdx == 0 ){
+        binding.percentage1.text = getString(R.string.index0O)
+        binding.percentage2.text = getString(R.string.index0C)
+        binding.percentage3.text = getString(R.string.index0E)
+        binding.percentage4.text = getString(R.string.index0A)
+        binding.percentage5.text = getString(R.string.index0N)
+    }
+    else if(maxIdx == 1){
+        binding.percentage1.text = getString(R.string.index1O)
+        binding.percentage2.text = getString(R.string.index1C)
+        binding.percentage3.text = getString(R.string.index1E)
+        binding.percentage4.text = getString(R.string.index1A)
+        binding.percentage5.text = getString(R.string.index1N)
+    }
+    else if(maxIdx == 2){
+        binding.percentage1.text = getString(R.string.index2O)
+        binding.percentage2.text = getString(R.string.index2C)
+        binding.percentage3.text = getString(R.string.index2E)
+        binding.percentage4.text = getString(R.string.index2A)
+        binding.percentage5.text = getString(R.string.index2N)
+    }
+    else if(maxIdx == 3){
+        binding.percentage1.text = getString(R.string.index3O)
+        binding.percentage2.text = getString(R.string.index3C)
+        binding.percentage3.text = getString(R.string.index3E)
+        binding.percentage4.text = getString(R.string.index3A)
+        binding.percentage5.text = getString(R.string.index3N)
     }
 
-
-    private fun localModel(){
-
-
-        val byteBuffer : ByteBuffer = ByteBuffer.allocateDirect(50*4)
-
-        val model = Model.newInstance(applicationContext)
-        // Creates inputs for reference.
-
-        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 50), DataType.FLOAT32)
-        inputFeature0.loadBuffer(byteBuffer)
-
-        // Runs model inference and gets result.
-        val outputs = model.process(inputFeature0)
-        val outputFeature0 = outputs.outputFeature0AsTensorBuffer.floatArray
-
-        binding.percentage1.text = outputFeature0[0].toString()
-        binding.percentage2.text = outputFeature0[1].toString()
-        binding.percentage3.text = outputFeature0[2].toString()
-        binding.percentage4.text = outputFeature0[3].toString()
-        binding.percentage5.text = outputFeature0[4].toString()
-
-        binding.result.text = getString(R.string.result_greeting)
-        binding.desc.text = getString(R.string.o_desc)
-        binding.sResult.text = getString(R.string.o_strength)
-        binding.wResult.text = getString(R.string.o_weakness)
-
-        // Releases model resources if no longer used.
-        model.close()
+    else{
+        binding.percentage1.text = getString(R.string.index4O)
+        binding.percentage2.text = getString(R.string.index4C)
+        binding.percentage3.text = getString(R.string.index4E)
+        binding.percentage4.text = getString(R.string.index4A)
+        binding.percentage5.text = getString(R.string.index4N)
     }
+
+}
+
+
 
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -104,5 +124,8 @@ class ResultActivity : AppCompatActivity() {
     override fun onBackPressed() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
+    }
+    companion object{
+        const val EXTRA_FLOAT = "traits"
     }
 }
